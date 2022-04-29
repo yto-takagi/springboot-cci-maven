e42bff75bbc86941c976c8d8cf393d41a5e76175
takagi yuto <yto.oct.14@gmail.com>
2022-04-29 18:50:26 +0900

    first commit

---
+++ src/main/java/com/cci/example/ccidemo/CcidemoApplication.java
@@ -0,0 +1,13 @@
+package com.cci.example.ccidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcidemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cci/example/ccidemo/CcidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cci.example.ccidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
